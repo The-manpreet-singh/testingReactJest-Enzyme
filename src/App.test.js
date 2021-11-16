a9f847ef5ecb726ab9ec75c054ffcd2dabb2c24c
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-11-16 17:47:36 +0530

    dinished btn and div

--- src/App.test.js
+++ src/App.test.js
@@ -5,26 +5,26 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-/** 
+/**
  * Factory function to create a ShallowWrapper for the App component.
  * @function setup
  * @param {object} props - Component props specific to this setup.
  * @param {any} state - Initial state for setup.
  * @returns {ShallowWrapper}
  */
-const setup = (props={}, state=null) => {
-  return shallow(<App {...props} />)
-}
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
 
 /**
  * Return ShallowWrapper containing node(s) with the given data-test value.
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within. 
- * @param {string} val - Value of data-test attribute for search. 
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for search.
  * @returns {ShallowWrapper}
  */
 const findByTestAttr = (wrapper, val) => {
-  return
-}
+  return wrapper.find(`[data-test="${val}"]`);
+};
 
 test("renders without error", () => {
   const wrapper = setup();
@@ -32,19 +32,22 @@ test("renders without error", () => {
   //console.log(wrapper.debug()); // this is very helpful debugging
   //expect(wrapper).toBeTruthy(); // mearns(true) wrapper would not be null, empty, and undefined.
   //expect(wrapper).toBeFalsy(); // mearns(false) wrapper would be null, empty, and undefined.
-  const appComponent = wrapper.find("[data-test='component-app']");
+  //const appComponent =  wrapper.find("[data-test='component-app']");
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 test("renders increment button", () => {
   const wrapper = setup();
-  const button = wrapper.find("[data-test='increment-button']");
+  // const button = wrapper.find("[data-test='increment-button']");
+  const button = findByTestAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
   const wrapper = setup();
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  //const counterDisplay = wrapper.find("[data-test='counter-display']")
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
 
